e18613b121346388e04efb3b0b97c1772d56e77a
mort <manuel.gonzalez.noriega@gmail.com>
2013-02-28 02:21:34 +0100

    CartoDB && renamed animalito to actor

--- lib/position.rb
+++ lib/position.rb
@@ -1,15 +1,19 @@
 class Position
-  
-  attr_reader :created_at, :animalito, :location, :venue
-  
-  def initialize(location, animalito, options = {})
-    
+
+  attr_reader :created_at, :actor, :location, :venue
+
+  def initialize(location, actor, options = {})
+
     @created_at = Time.now
-    @animalito = animalito
+    @actor = actor
     @location = location
     @venue = options[:venue]
-    
-    @location.add_occupant(animalito)
+
+    @location.add_occupant(actor)
+
+
+    CartoDB::Connection.insert_row 'animalitos_positions', :name => @actor.name, :lat => @location.lat, :lon => @location.lon, :description => @actor.class.to_s
+
   end
-  
+
 end
