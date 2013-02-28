7bcfb78e16449bd87b7c320f25a14893441225f1
mort <manuel.gonzalez.noriega@gmail.com>
2013-02-28 02:24:06 +0100

    Player fixes

--- lib/player.rb
+++ lib/player.rb
@@ -1,5 +1,5 @@
 class Player
-  include Observable 
+  include Observable
   include Movable
 
 	attr_accessor :animalito, :name
@@ -9,52 +9,50 @@ class Player
 		@animalito = nil
 		@positions = []
     @bound = false
-    
+
 		add_observer Announcer.new
 	end
-	
+
 	def hatch
 	  return if @bound
-    bond_with(Animalito.new)
-  end
-	
+        bond_with(Animalito.new)
+      end
+
 	def bond_with(animalito)
 	  raise 'Already bonded' if @bound
 
 		@animalito = animalito
 		@animalito.bond_with(self)
 		@bound = true
-		
+
 		# Player should follow its animalito
     #follow(@animalito)
-		
-		changed    
+
+		changed
 		notify_observers self, "#{to_param} is now bonded with #{animalito.to_param}", :bond
-		
+
 		animalito
 	end
-	
+
 	def unbond
 	  @animalito = nil
   end
-  
+
   def checkin(venue)
-    
+
     lat = venue.location.lat
     lon = venue.location.lng
-    
-    move_to(Location.new(lat,lon), {:with_animalito => false, :venue => venue})
-    @animalito.checkin(venue)
-    
+
+    move_to(Location.new(lat,lon), {:checkin => true, :venue => venue})
   end
-	
-	def move_to(location, options = {})
+
+	def move_to(location, options = {:with_animalito => true})
 	  super
-	  @animalito.move_to(location, options) if options[:with_animalito] && @animalito.leashed 
+	  @animalito.move_to(location, options) if @animalito && options[:with_animalito] && @animalito.leashed
   end
-	
+
 	def to_param
     name
   end
-	
+
 end
